56b5f48b3351aba2fe9bad7adb6c0e6459599afa
rmckinney <ryangmckinney@hotmail.com>
2017-05-15 11:37:01 -0700

    models

--- src/java/com/stanchefskitchen/data/models/Order.java
+++ src/java/com/stanchefskitchen/data/models/Order.java
@@ -1,9 +1,22 @@
 package com.stanchefskitchen.data.models;
 
 /**
- *
- * @author Tyler Wong
+ * 
+ * @author Ryan
  */
 public class Order {
+    public enum Status {
+        PREPARING, READY, DELIVERING, COMPLETE;
+    }
     
+    public int id;
+    public int customerId;
+    public int billId;
+    public Status orderStatus;
+    
+    public Order (int id, int customerId, int billId, Status orderStatus) {
+        this.customerId = customerId;
+        this.billId = billId;
+        this.orderStatus = orderStatus;
+    }
 }
---
+++ src/java/com/stanchefskitchen/data/models/OrderItem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stanchefskitchen.data.models;
+
+/**
+ *
+ * @author Ryan
+ */
+public class OrderItem {
+    public int id;
+    public int orderId;
+    public String itemName;
+    public int quantity;
+    
+    public OrderItem (int id, int orderId, String itemName, int quantity) {
+        this.id = id;
+        this.orderId = orderId;
+        this.itemName = itemName;
+        this.quantity = quantity;
+    }
+}
