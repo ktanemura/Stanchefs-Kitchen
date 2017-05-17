de1154758fcd62f1da12830690d16104728ab55c
rmckinney <ryangmckinney@hotmail.com>
2017-05-17 11:17:52 -0700

    changed format

--- src/java/com/stanchefskitchen/data/models/Order.java
+++ src/java/com/stanchefskitchen/data/models/Order.java
@@ -5,16 +5,12 @@ package com.stanchefskitchen.data.models;
  * @author Ryan
  */
 public class Order {
-    public enum Status {
-        PREPARING, READY, DELIVERING, COMPLETE;
-    }
-    
     public int id;
     public int customerId;
     public int billId;
-    public Status orderStatus;
+    public OrderStatus orderStatus;
     
-    public Order (int id, int customerId, int billId, Status orderStatus) {
+    public Order (int id, int customerId, int billId, OrderStatus orderStatus) {
         this.customerId = customerId;
         this.billId = billId;
         this.orderStatus = orderStatus;
