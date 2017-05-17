aeddbc11b3b8e945ffa632d36f0db05985a3b80b
brittanyberlanga <brittany.berlanga@gmail.com>
2017-05-17 10:13:39 -0700

    Added meal type to menu item

--- src/java/com/stanchefskitchen/data/models/MenuItem.java
+++ src/java/com/stanchefskitchen/data/models/MenuItem.java
@@ -5,14 +5,17 @@ package com.stanchefskitchen.data.models;
  * @author Brittany Berlanga
  */
 public class MenuItem {
-    //TODO add meal type and item type
+    //TODO add item type
     public final String name;
     public final double price;
     public final String description;
+    public final MealType mealType;
     
-    public MenuItem(String name, double price, String description) {
+    public MenuItem(String name, double price, String description, 
+            MealType mealType) {
         this.name = name;
         this.price = price;
         this.description = description;
+        this.mealType = mealType;
     }
 }
