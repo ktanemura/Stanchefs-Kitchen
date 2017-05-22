342102a45c66f2e61eee6d64118a84a0bd90417a
ktanemura <kyletanemura@gmail.com>
2017-05-21 21:01:43 -0700

    Updates ItemTypeProvider
    -Now can add, update, and delete ItemTypes

--- src/java/com/stanchefskitchen/data/providers/ItemTypeProvider.java
+++ src/java/com/stanchefskitchen/data/providers/ItemTypeProvider.java
@@ -24,16 +24,41 @@ import java.util.logging.Logger;
 public class ItemTypeProvider {
     private static Connection connection = DatabaseProvider.getConnection();
     
-    private static final String GET_TYPE = "select * from ItemType where id = ?;";
+    private static final String GET_TYPE_ID = "select * from ItemType where id = ?;";
+    private static final String GET_TYPE_NAME = "select * from ItemType where name = ?;";
     private static final String GET_MENUITEM_TYPES = "select i.id, i.name "
             + "from ItemType i join MenuItem m "
             + "on m.itemTypeId = i.id where m.name = ?;";
-    
-    public static ItemType getType(int itemTypeId) {
+    private static final String ADD_TYPE = "insert into ItemType(name) values(?);";
+    private static final String UPDATE_TYPE = "update ItemType set name = ? where id = ?;";
+    private static final String DELETE_TYPE = "deleteom ItemType where id = ?;";
+    public static ItemType getTypeById(int itemTypeId) {
         ItemType type = null;
         try {
-            PreparedStatement statement = connection.prepareStatement(GET_TYPE);
+            PreparedStatement statement = connection.prepareStatement(GET_TYPE_ID);
             statement.setInt(1, itemTypeId);
+            ResultSet results = statement.executeQuery();
+            
+            if (results.isBeforeFirst()) {
+                results.next();
+                type = new ItemType(
+                    results.getInt(ItemType.ID),
+                    results.getString(ItemType.NAME));
+            }
+            
+            return type;
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemTypeProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return type;
+    }
+    
+    public static ItemType getTypeByName(String name) {
+        ItemType type = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_TYPE_ID);
+            statement.setString(1, name);
             ResultSet results = statement.executeQuery();
             
             if (results.isBeforeFirst()) {
@@ -69,4 +94,34 @@ public class ItemTypeProvider {
         return types;
     }
     
+    public static void addItemType(String name) {
+        try {
+            PreparedStatement s = connection.prepareStatement(ADD_TYPE);
+            s.setString(1, name);
+            s.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemTypeProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void updateItemType(int id, String newName) {
+        try {
+            PreparedStatement s = connection.prepareStatement(UPDATE_TYPE);
+            s.setInt(2, id);
+            s.setString(1, newName);
+            s.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemTypeProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void deleteItemType(int id) {
+        try {
+            PreparedStatement s = connection.prepareStatement(DELETE_TYPE);
+            s.setInt(1, id);
+            s.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemTypeProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
