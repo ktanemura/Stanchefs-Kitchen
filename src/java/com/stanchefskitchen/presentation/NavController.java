3bc265c251ae74a6203bc50ef617c10d3230e810
Tyler Wong <tbwong3@gmail.com>
2017-06-02 11:38:20 -0700

    Added login/signup

--- src/java/com/stanchefskitchen/presentation/NavController.java
+++ src/java/com/stanchefskitchen/presentation/NavController.java
@@ -1,9 +1,61 @@
 package com.stanchefskitchen.presentation;
 
+import com.stanchefskitchen.presentation.login.Session;
+import java.io.Serializable;
+
 /**
  *
- * @author Tyler Wong
+ * @author Brittany Berlanga
  */
-public class NavController {
+public class NavController implements Serializable {
+    public static final String LOGIN = "login";
+    public static final String SIGNUP = "signup";
+    public static final String ADMIN_HOME = "admin_home";
+    public static final String EMPL_HOME = "empl_home";
+    public static final String CUS_HOME = "cus_home";
+    public static final String CREATE_ACCOUNT = "create_account";
     
+    private Session userSession;
+    
+    /**
+     * Creates a new instance of NavController
+     */
+    public NavController() {
+    }
+    
+    public Session getUserSession()
+    {
+        return userSession;
+    }
+
+    public void setUserSession(Session userSession)
+    {
+        this.userSession = userSession;
+    }
+       
+    public String goToLogin() {
+        return LOGIN;
+    }
+    
+    public String goToCreateAccount() {
+        return SIGNUP;
+    }
+    
+    public String goToAccountHome() {
+        switch (userSession.getAccount().type) {
+            case administrator:
+                return ADMIN_HOME;
+            case employee:
+                return EMPL_HOME;
+            case customer:
+                return CUS_HOME;
+            default:
+                return LOGIN;
+        }
+    }
+    
+    public String logout() {
+        userSession.logout();
+        return LOGIN;
+    }
 }
---
+++ src/java/com/stanchefskitchen/presentation/home/HeaderController.java
@@ -0,0 +1,32 @@
+package com.stanchefskitchen.presentation.home;
+
+import com.stanchefskitchen.data.models.Account;
+import com.stanchefskitchen.presentation.login.Session;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Brittany Berlanga
+ */
+public class HeaderController implements Serializable {
+    private static final String LOGGED_IN_INFO = "Logged in as: %s %s";
+    private Session userSession;
+    private String loggedInInfo;
+    
+    public HeaderController() {}
+    
+    public void setUserSession(Session userSession) {
+        this.userSession = userSession;
+    }
+    
+    public Session getUserSession() {
+        return userSession;
+    }
+    
+    public String getLoggedInInfo() {
+        Account account = userSession.getAccount();
+        this.loggedInInfo = String.format(LOGGED_IN_INFO, account.firstName, 
+                account.lastName);
+        return loggedInInfo;
+    }
+}
