56e606025192bb0a4ddd136262e7bb8305519a51
brittanyberlanga <brittany.berlanga@gmail.com>
2017-06-09 11:09:32 -0700

    Add add item in nav controller

--- src/java/com/stanchefskitchen/presentation/NavController.java
+++ src/java/com/stanchefskitchen/presentation/NavController.java
@@ -16,6 +16,7 @@ public class NavController implements Serializable {
     public static final String MENU = "menu";
     public static final String CREATE_ACCOUNT = "create_account";
     public static final String SHOPPING_CART = "shopping_cart";
+    public static final String ADD_ITEM = "add_order_item";
     
     private Session userSession;
     
