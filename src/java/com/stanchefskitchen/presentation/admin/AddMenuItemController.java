8e55d079c1c76699574d1596bf501b729c76e343
rmckinney <ryangmckinney@hotmail.com>
2017-06-09 17:38:48 -0700

    add menu item working no validation yet

---
+++ src/java/com/stanchefskitchen/presentation/admin/AddMenuItemController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stanchefskitchen.presentation.admin;
+
+import com.stanchefskitchen.data.models.ItemType;
+import com.stanchefskitchen.data.models.MenuItem;
+import com.stanchefskitchen.data.models.MenuItemType;
+import com.stanchefskitchen.data.providers.ItemTypeProvider;
+import com.stanchefskitchen.data.providers.MenuItemProvider;
+import javax.faces.component.UIInput;
+
+/**
+ *
+ * @author Ryan
+ */
+public class AddMenuItemController {
+    private UIInput itemNameUI;
+    private UIInput priceUI;
+    private UIInput descriptionUI;
+    private UIInput itemTypeUI;
+
+    public void addMenuItem() {
+        String itemName = itemNameUI.getLocalValue().toString().trim();
+        double price = Double.parseDouble(priceUI.getLocalValue().toString());
+        String description = descriptionUI.getLocalValue().toString().trim();
+        String itemTypeStr = itemTypeUI.getLocalValue().toString().trim();
+    
+        // Add menu item
+        MenuItem menuItem = new MenuItem(itemName, price, description, true);
+        if (!MenuItemProvider.addMenuItem(menuItem)) {
+            System.out.println("Couldn't add menu item"); // throws error
+            return;
+        }
+        
+        // Create or get item type id
+        ItemType itemType = ItemTypeProvider.getTypeByName(itemTypeStr);
+        int itemTypeId;
+        if (itemType == null) {
+            itemTypeId = ItemTypeProvider.addItemType(itemTypeStr);
+        }
+        else {
+            itemTypeId = itemType.getId();
+        }
+        
+        if (!MenuItemProvider.addMenuItemType(menuItem, new MenuItemType(itemName, itemTypeId))) {
+            // throw error
+        }
+    }
+
+    public UIInput getItemNameUI() {
+        return itemNameUI;
+    }
+
+    public void setItemNameUI(UIInput itemNameUI) {
+        this.itemNameUI = itemNameUI;
+    }
+
+    public UIInput getPriceUI() {
+        return priceUI;
+    }
+
+    public void setPriceUI(UIInput priceUI) {
+        this.priceUI = priceUI;
+    }
+
+    public UIInput getDescriptionUI() {
+        return descriptionUI;
+    }
+
+    public void setDescriptionUI(UIInput descriptionUI) {
+        this.descriptionUI = descriptionUI;
+    }
+
+    public UIInput getItemTypeUI() {
+        return itemTypeUI;
+    }
+
+    public void setItemTypeUI(UIInput itemTypeUI) {
+        this.itemTypeUI = itemTypeUI;
+    }
+   
+}
---
+++ src/java/com/stanchefskitchen/presentation/admin/AdminHomeController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stanchefskitchen.presentation.admin;
+
+import com.stanchefskitchen.data.models.MenuItem;
+import com.stanchefskitchen.data.providers.MenuItemProvider;
+import java.util.*;
+
+/**
+ *
+ * @author Ryan
+ */
+public class AdminHomeController {
+    private final String EMPL_ACCOUNT = "admin_create_employee";
+    private final String ADD_MENUITEM = "admin_add_menuitem";
+    private final String EDIT_MENUITEM = "admin_edit_menuitem";
+    private EditMenuItemController editMenuItemController;
+    
+    public String goToCreateEmplAccount() {
+        return EMPL_ACCOUNT;
+    }
+    
+    public String goToEditMenuItem() {
+        // fer testing
+        List<MenuItem> items = MenuItemProvider.getAllAvailableMenuItems();
+        for (MenuItem cur_item : items) {
+            if (cur_item.name.equals("Mozzarella Pizza")) {
+                editMenuItemController.menuItem = cur_item;
+            }
+        }
+        
+        return EDIT_MENUITEM;
+    }
+
+    public String goToAddMenuItem() {
+        return ADD_MENUITEM;
+    }
+    
+    public EditMenuItemController getEditMenuItemController() {
+        return editMenuItemController;
+    }
+
+    public void setEditMenuItemController(EditMenuItemController editMenuItemController) {
+        this.editMenuItemController = editMenuItemController;
+    }
+}
